77424c9efb4a166062f0ae7cc45f199acabfda51
10451 <c1045132383@gmail.com>
2022-05-22 23:29:05 +0800

    使用页码控件

--- src/MainWidget/mainwidget.cc
+++ src/MainWidget/mainwidget.cc
@@ -14,12 +14,13 @@ MainWidget::MainWidget() : sql_(new M_Sql) {
   auto *main_layout = new QHBoxLayout(this);
 
   //使用函数功能穿件左右两侧控件
-  auto *left_table_box = CreateStuMess();
-  auto *right_table_box = CreateMenu();
+  auto left_table_box = CreateStuMess(50);
+  auto right_table_box = CreateMenu();
 
   //左右两侧控件加入主布局
   main_layout->addWidget(left_table_box, 3);
   main_layout->addWidget(right_table_box, 2);
+
 }
 
 MainWidget::~MainWidget() {
@@ -32,7 +33,13 @@ MainWidget::~MainWidget() {
  * 创建学生信息窗口,即左侧控件
  * @return
  */
-QGroupBox *MainWidget::CreateStuMess() {
+QGroupBox *MainWidget::CreateStuMess(int page_size) {
+  //设置分页参数
+  this->page_size_ = page_size;
+  this->current_page_ = 0;
+  int total_page = sql_->GetTotalNum() / page_size_;
+  this->total_page_ = total_page + (sql_->GetTotalNum() % page_size_ == 0 ? 0 : 1);
+
   auto box = new QGroupBox("Student Information");
   table_widget_ = new QTableWidget(box);
   table_widget_->setSelectionBehavior(QAbstractItemView::SelectRows);
@@ -40,10 +47,20 @@ QGroupBox *MainWidget::CreateStuMess() {
   //设置不可编辑
   table_widget_->setEditTriggers(QAbstractItemView::NoEditTriggers);
   table_widget_->setHorizontalHeaderLabels(QStringList() << "ID" << "Name" << "Gender" << "Age");
+  //分页控件
+  paging_widget_ = new PagingWidget(box);
+  paging_widget_->SetCurrentPage(current_page_);
+
   FlushTable();
-  auto AutoHBoxLayout = new QHBoxLayout(box);
-  AutoHBoxLayout->addWidget(table_widget_);
-  box->setLayout(AutoHBoxLayout);
+  auto box_layout = new QVBoxLayout(box);
+  auto table_layout = new QHBoxLayout(box);
+  auto paging_layout = new QHBoxLayout(box);
+  table_layout->addWidget(table_widget_);
+  paging_layout->addWidget(paging_widget_);
+  paging_layout->setAlignment(Qt::AlignCenter);
+  box_layout->addLayout(table_layout);
+  box_layout->addLayout(paging_layout);
+  box->setLayout(box_layout);
 
   //信号槽连接
   //当用户点击表格中的某个单元格时，触发cellClicked信号，更新列表中学生信息
@@ -52,6 +69,17 @@ QGroupBox *MainWidget::CreateStuMess() {
   connect(table_widget_, &QTableWidget::cellChanged, this, &MainWidget::FlushListWidget);
   //当用户双击表格中某一行时，触发cellDoubleClicked信号，调用ChangeStuMessItem函数
   connect(table_widget_, &QTableWidget::itemDoubleClicked, this, &MainWidget::ChangeStuMessItem);
+  //当用户输入页码时，调用FlushTable函数
+  connect(paging_widget_, &PagingWidget::PageChanged, this, &MainWidget::FlushTableFromPageNum);
+  //当用户点击第一页是，触发FirstPage信号，调用FirstPage函数
+  connect(paging_widget_, &PagingWidget::FirstPage, this, &MainWidget::FirstPage);
+  //当用户点击最后一页是，触发LastPage信号，调用LastPage函数
+  connect(paging_widget_, &PagingWidget::LastPage, this, &MainWidget::LastPage);
+  //当用户点击上一页是，触发PrevPage信号，调用PrevPage函数
+  connect(paging_widget_, &PagingWidget::PrevPage, this, &MainWidget::PrevPage);
+  //当用户点击下一页是，触发NextPage信号，调用NextPage函数
+  connect(paging_widget_, &PagingWidget::NextPage, this, &MainWidget::NextPage);
+
   return box;
 }
 
@@ -62,7 +90,7 @@ QGroupBox *MainWidget::CreateStuMess() {
 QGroupBox *MainWidget::CreateMenu() {
   auto box = new QGroupBox("Student Details");
   //功能面板
-  auto VBoxLayout = new QVBoxLayout(box);
+  auto box_layout = new QVBoxLayout(box);
   list_widget_ = new QListWidget(box);
   //按钮控件
   auto Buts = new QGridLayout;
@@ -77,9 +105,9 @@ QGroupBox *MainWidget::CreateMenu() {
   Buts->addWidget(find_stu_edit_, 1, 0, 1, 0);
   Buts->addWidget(export_button_, 2, 0, 1, 0);
 
-  VBoxLayout->addWidget(list_widget_, 2);
-  VBoxLayout->addLayout(Buts, 3);
-  box->setLayout(VBoxLayout);
+  box_layout->addWidget(list_widget_, 2);
+  box_layout->addLayout(Buts, 3);
+  box->setLayout(box_layout);
 
   //信号槽连接
   //点击添加按钮时，执行AddStuBox函数
@@ -88,7 +116,7 @@ QGroupBox *MainWidget::CreateMenu() {
   connect(del_stu_button_, &QPushButton::clicked, this, &MainWidget::DeleteStudent);
   //当在查找框中输入学生姓名后，调用FindStuMess函数
   connect(find_stu_edit_, &QLineEdit::returnPressed, this, &MainWidget::FindStuMess);
-  //点击导出按钮时，执行ExportStuMess函数
+  //当用户击导出按钮时，执行ExportStuMess函数
   connect(export_button_, &QPushButton::clicked, this, &MainWidget::Export);
   return box;
 }
@@ -125,13 +153,13 @@ void MainWidget::ModifyStuBox(QStringList &list) {
 }
 
 /*!
- * 从数据库中获得数据，刷新学生表单
+ * @brief 根据页码从数据库中获得数据，刷新学生表单
  */
 void MainWidget::FlushTable() {
+  CalulateTotalPage();
   //更新表格前，断开cellChange信号所关联的槽
   disconnect(table_widget_, &QTableWidget::cellChanged, nullptr, nullptr);
-
-  auto Students = sql_->SelectAll();
+  auto Students = sql_->SelectPage(current_page_, page_size_);
   table_widget_->setRowCount(0);
   for (auto &stu: Students) {
     table_widget_->setRowCount(table_widget_->rowCount() + 1);
@@ -145,17 +173,63 @@ void MainWidget::FlushTable() {
   connect(table_widget_, &QTableWidget::cellChanged, this, &MainWidget::ChangeStuMess);
   connect(table_widget_, &QTableWidget::cellChanged, this, &MainWidget::FlushListWidget);
 }
-
+/*!
+ * @brief 根据输入页码从数据库中获得数据，刷新学生表单
+ */
+void MainWidget::FlushTableFromPageNum(int current_page) {
+  //防止页码超出范围
+  if (current_page == current_page_)
+    return;
+  if (current_page < 0)
+    current_page = 0;
+  if (current_page > total_page_)
+    current_page = total_page_ - 1;
+  //更新页码
+  current_page_ = current_page;
+  paging_widget_->SetCurrentPage(current_page_);
+  FlushTable();
+}
+/*!
+ * @brief 跳转到下一页
+ */
+void MainWidget::NextPage() {
+  if (current_page_ < total_page_) {
+    ++current_page_;
+    paging_widget_->SetCurrentPage(current_page_);
+    FlushTable();
+  }
+}
+/*!
+ * @brief 跳转到上一页
+ */
+void MainWidget::PrevPage() {
+  if (current_page_ > 0) {
+    --current_page_;
+    paging_widget_->SetCurrentPage(current_page_);
+    FlushTable();
+  }
+}
+/*!
+ * @brief 跳转到第一页
+ */
+void MainWidget::FirstPage() {
+  current_page_ = 0;
+  paging_widget_->SetCurrentPage(current_page_);
+  FlushTable();
+}
+/*!
+ * @brief 跳转到最后一页
+ */
+void MainWidget::LastPage() {
+  current_page_ = total_page_ - 1;
+  paging_widget_->SetCurrentPage(current_page_);
+  FlushTable();
+}
 /**
  * 刷新功能列表
  * @param row
  */
 void MainWidget::FlushListWidget(int row) {
-  /*!
-   * 更新功能面板列表中的学生信息
-   */
-  //当有信息的时候，直接修改，否则添加
-
   if (list_widget_->count() > 0) {
     list_widget_->item(0)->setText("ID:" + table_widget_->item(row, 0)->text());
     list_widget_->item(1)->setText("Name:" + table_widget_->item(row, 1)->text());
@@ -247,9 +321,22 @@ void MainWidget::Export() {
   auto Students = sql_->SelectAll();
   std::ofstream outfile("student.csv", std::ios::out | std::ios::trunc);
   outfile << "ID,Name,Gender,Age" << std::endl;
-  for (const auto &student: Students) {
-    outfile << student.GetInfo() << std::endl;
+  for (auto &student: Students) {
+    outfile << student.GetInfo();
   }
   outfile.close();
   QMessageBox::information(this, "Success", "CSV file has been export!");
+}
+/*!
+ * @brief 计算更新总页数
+ */
+void MainWidget::CalulateTotalPage() {
+  int total_page = sql_->GetTotalNum() / page_size_ + (sql_->GetTotalNum() % page_size_ == 0 ? 0 : 1);
+  //如果总页数改变，则更新总页数，并且将当前页更新为最后一页
+  if (this->total_page_ != total_page) {
+    this->total_page_ = total_page;
+    current_page_ = total_page_ - 1;
+    paging_widget_->SetCurrentPage(current_page_);
+  }
+  paging_widget_->SetInfo(sql_->GetTotalNum(), total_page_);
 }
--- src/MainWidget/mainwidget.h
+++ src/MainWidget/mainwidget.h
@@ -14,6 +14,7 @@
 #include <QHBoxLayout>
 #include <fstream>
 #include "../EditStuMessBox/editstumessbox.h"
+#include "../PagingWidget/pagingwidget.h"
 #include "../Config/common.h"
 #include "../Sql/msql.h"
 
@@ -22,10 +23,17 @@ class MainWidget : public QWidget {
  public:
   MainWidget();
   ~MainWidget() override;
+ private:
+  void CalulateTotalPage();
   void AddStuBox();
   void ModifyStuBox(QStringList &list);
  public slots:
   void FlushTable();
+  void FlushTableFromPageNum(int current_page);
+  void FirstPage();
+  void PrevPage();
+  void NextPage();
+  void LastPage();
   void FlushListWidget(int row);
   void DeleteStudent();
   void FindStuMess();
@@ -34,12 +42,17 @@ class MainWidget : public QWidget {
   void Export();
 
  private:
-  QGroupBox *CreateStuMess();
+  QGroupBox *CreateStuMess(int page_size);
   QGroupBox *CreateMenu();
 
  private:
+  //分页参数
+  int total_page_{};
+  int current_page_{};
+  int page_size_{};
   //主窗口学生面板
   QTableWidget *table_widget_{};
+  PagingWidget *paging_widget_{};
   //主窗口功能面板
   QListWidget *list_widget_{};
   //主窗口添加、删除,导出按钮
