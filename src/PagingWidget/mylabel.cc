e8b9911e98447cf6d8a1ed88d297792daacc94c7
10451 <c1045132383@gmail.com>
2022-06-18 16:20:26 +0800

    更新注释

--- src/PagingWidget/mylabel.cc
+++ src/PagingWidget/mylabel.cc
@@ -1,13 +1,12 @@
 //
 // Created by 10451 on 2022/5/22.
 //
-//为label添加Clicked信号
+// 为label添加Clicked信号
 
 #include "mylabel.h"
-MyLabel::MyLabel(QWidget *parent) : QLabel(parent) {
-
-}
-void MyLabel::mousePressEvent(QMouseEvent *event) {
-  emit Clicked();
-}
-
+MyLabel::MyLabel(QWidget *parent) : QLabel(parent) {}
+/*!
+ * @brief 重写该函数，当鼠标点击label时，发送Clicked信号
+ * @param event
+ */
+void MyLabel::mousePressEvent(QMouseEvent *event) { emit Clicked(); }
--- src/PagingWidget/mylabel.h
+++ src/PagingWidget/mylabel.h
@@ -1,20 +1,22 @@
 //
 // Created by 10451 on 2022/5/22.
 //
+/*label没有点击信号，因此写一个带有Clicked信号的label控件*/
 
 #ifndef STUDENTMANAGE_SRC_PAGINGWIDGET_MYLABEL_H_
 #define STUDENTMANAGE_SRC_PAGINGWIDGET_MYLABEL_H_
-#include <QWidget>
 #include <QLabel>
+#include <QWidget>
 class MyLabel : public QLabel {
- Q_OBJECT
- public:
+  Q_OBJECT
+public:
   explicit MyLabel(QWidget *parent = nullptr);
   ~MyLabel() override = default;
- signals:
+signals:
   void Clicked();
- protected:
+
+protected:
   void mousePressEvent(QMouseEvent *event) override;
 };
 
-#endif //STUDENTMANAGE_SRC_PAGINGWIDGET_MYLABEL_H_
+#endif // STUDENTMANAGE_SRC_PAGINGWIDGET_MYLABEL_H_
